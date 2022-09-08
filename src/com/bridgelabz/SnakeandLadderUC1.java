ba1934990312cefc93affccb14391a9d5b74ec33
Maxen24 <mshubham242@gmail.com>
2022-09-09 00:44:49 +0530

    Added Snake and ladder UC3

--- src/com/bridgelabz/SnakeandLadderUC1.java
+++ src/com/bridgelabz/SnakeandLadderUC1.java
@@ -15,10 +15,27 @@ public class SnakeandLadderUC1 {
 	 */
 	public static void main(String[] args) {
 		int position =0;
-		System.out.println("Player one position is"+" "+ position);
 		Random rand=new Random();
 		int rolldie=rand.nextInt(6)+1;
 		System.out.println("Dice Number"+" "+rolldie);
+				int options =rand.nextInt(3);
+				int temp;
+		System.out.println("Check Options"+" "+options);
+		if (options ==1) {
+			System.out.println("ladder");
+			position=(position+rolldie);
+			System.out.println("Current Position is "+" "+position);
+		}else if(options==0) {
+			System.out.println("No play");
+			System.out.println("Current Position is"+" "+position);
+		}else if (options==2){
+			System.out.println("Snake");
+			temp=(position-rolldie);
+			if (temp<0) {
+				position=0;
+			System.out.println("Current Position is"+" "+position);
+			
+		}
 	}
-
+	}
 }
