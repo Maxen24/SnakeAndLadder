b902ad7ca24937ca0269974bbd5b35c01621bff7
Maxen24 <mshubham242@gmail.com>
2022-09-08 16:00:30 +0530

    Started Snake and Ladder UC1

---
+++ src/com/bridgelabz/SnakeandLadderUC1.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+/**
+ * @author mshub
+ *
+ */
+import java.lang.Math;
+public class SnakeandLadderUC1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int position =0;
+		System.out.println("Player one position is"+" "+ position);
+
+	}
+
+}
