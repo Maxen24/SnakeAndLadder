09eac85c6ac1bc5b912ca4a3a18455066810ceca
Maxen24 <mshubham242@gmail.com>
2022-09-08 17:34:08 +0530

    Added Snake and Ladder UC2

--- src/com/bridgelabz/SnakeandLadderUC1.java
+++ src/com/bridgelabz/SnakeandLadderUC1.java
@@ -7,7 +7,7 @@ package com.bridgelabz;
  * @author mshub
  *
  */
-import java.lang.Math;
+import java.util.Random;
 public class SnakeandLadderUC1 {
 
 	/**
@@ -16,7 +16,9 @@ public class SnakeandLadderUC1 {
 	public static void main(String[] args) {
 		int position =0;
 		System.out.println("Player one position is"+" "+ position);
-
+		Random rand=new Random();
+		int rolldie=rand.nextInt(6)+1;
+		System.out.println("Dice Number"+" "+rolldie);
 	}
 
 }
