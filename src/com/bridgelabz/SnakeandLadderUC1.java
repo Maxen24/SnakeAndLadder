5938fcd428c71c55a803ea40aba0ed04e0eeda92
Maxen24 <mshubham242@gmail.com>
2022-09-09 14:08:33 +0530

    Added Snake and Ladder UC5

--- src/com/bridgelabz/SnakeandLadderUC1.java
+++ src/com/bridgelabz/SnakeandLadderUC1.java
@@ -26,17 +26,82 @@ public class SnakeandLadderUC1 {
 			System.out.println("ladder");
 			position=(position+rolldie);
 			System.out.println("Current Position is "+" "+position);
+			switch(position) {
+			case 104:
+				position=position-rolldie;
+				System.out.println("Current Position is"+" "+position);
+				break;
+			case 106:
+				position=position-rolldie;
+				System.out.println("Current Position is"+" "+position);
+				break;
+
+			case 105:
+				position=position-rolldie;
+				System.out.println("Current Position is"+" "+position);
+				break;
+			case 103:
+				position=position-rolldie;
+				System.out.println("Current Position is"+" "+position);
+				break;
+
+			case 102:
+				position=position-rolldie;
+				System.out.println("Current Position is"+" "+position);
+				break;
+
+			case 101:
+				position=position-rolldie;
+				System.out.println("Current Position is"+" "+position);
+				break;
+			case 100:
+				System.out.println("Player won the game"+" "+position);
+
+			}
 		}else if(options==0) {
 			System.out.println("No play");
 			System.out.println("Current Position is"+" "+position);
+			switch(position) {
+			case 104:
+				position=position-rolldie;
+				System.out.println("Current Position is"+" "+position);
+
+			case 106:
+				position=position-rolldie;
+				System.out.println("Current Position is"+" "+position);
+				break;
+
+			case 105:
+				position=position-rolldie;
+				System.out.println("Current Position is"+" "+position);
+				break;
+			case 103:
+				position=position-rolldie;
+				System.out.println("Current Position is"+" "+position);
+				break;
+			case 102:
+				position=position-rolldie;
+				System.out.println("Current Position is"+" "+position);
+				break;
+			case 101:
+				position=position-rolldie;
+				System.out.println("Current Position is"+" "+position);
+				break;
+			case 100:
+				System.out.println("Player won the game"+" "+position);
+				break;
+			}
 		}else if (options==2){
 			System.out.println("Snake");
 			temp=(position-rolldie);
+			System.out.println("Current position"+" "+position);
 			if (temp<0) {
 				position=0;
 			System.out.println("Current Position is"+" "+position);
 			}
+			}
 	    }
+		
 	}
 }
-}
+	
