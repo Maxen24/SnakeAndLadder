dc5fb616084cb94fbd0f39b86ae29855a719cab2
Maxen24 <mshubham242@gmail.com>
2022-09-09 21:33:12 +0530

    refactor Snake and Ladder UC5

--- src/com/bridgelabz/SnakeandLadderUC1.java
+++ src/com/bridgelabz/SnakeandLadderUC1.java
@@ -15,77 +15,80 @@ public class SnakeandLadderUC1 {
 	 */
 	public static void main(String[] args) {
 		int position =0;
+		int sum=0;
 		for(;position<=99;) {
 		Random rand=new Random();
 		int rolldie=rand.nextInt(6)+1;
 		System.out.println("Dice Number"+" "+rolldie);
+		sum=sum+rolldie;
 				int options =rand.nextInt(3);
-				int temp;
 		System.out.println("Check Options"+" "+options);
 		if (options ==1) {
 			System.out.println("ladder");
-			position=(position+rolldie);
-			System.out.println("Current Position is "+" "+position);
+			position=position+rolldie;
+			System.out.println("Current Position is"+" "+position);
 			switch(position) {
 			case 104:
 				position=position-rolldie;
-				System.out.println("Current Position is"+" "+position);
+				System.out.println("Previous Position is"+" "+position);
 				break;
 			case 106:
 				position=position-rolldie;
-				System.out.println("Current Position is"+" "+position);
+				System.out.println("Previous Position is"+" "+position);
 				break;
 
 			case 105:
 				position=position-rolldie;
-				System.out.println("Current Position is"+" "+position);
+				System.out.println("Previous Position is"+" "+position);
 				break;
 			case 103:
 				position=position-rolldie;
-				System.out.println("Current Position is"+" "+position);
+				System.out.println("Previous Position is"+" "+position);
 				break;
 
 			case 102:
 				position=position-rolldie;
-				System.out.println("Current Position is"+" "+position);
+				System.out.println("Previous Position is"+" "+position);
 				break;
 
 			case 101:
 				position=position-rolldie;
-				System.out.println("Current Position is"+" "+position);
+				System.out.println("Previous Position is"+" "+position);
 				break;
 			case 100:
 				System.out.println("Player won the game"+" "+position);
-
+				System.out.println("Sum of Dice"+" "+ sum);
+				break;
 			}
+			
 		}else if(options==0) {
 			System.out.println("No play");
 			System.out.println("Current Position is"+" "+position);
 			switch(position) {
 			case 104:
 				position=position-rolldie;
-				System.out.println("Current Position is"+" "+position);
+				System.out.println("Previous Position is"+" "+position);
 
 			case 106:
 				position=position-rolldie;
-				System.out.println("Current Position is"+" "+position);
+				System.out.println("Previous Position is"+" "+position);
 				break;
 
 			case 105:
 				position=position-rolldie;
-				System.out.println("Current Position is"+" "+position);
+				System.out.println("Previous Position is"+" "+position);
 				break;
 			case 103:
 				position=position-rolldie;
-				System.out.println("Current Position is"+" "+position);
+				System.out.println("Previous Position is"+" "+position);
 				break;
 			case 102:
 				position=position-rolldie;
-				System.out.println("Current Position is"+" "+position);
+				System.out.println("Previous Position is"+" "+position);
 				break;
 			case 101:
 				position=position-rolldie;
-				System.out.println("Current Position is"+" "+position);
+				System.out.println("Previous Position is"+" "+position);
 				break;
 			case 100:
 				System.out.println("Player won the game"+" "+position);
@@ -93,15 +96,14 @@ public class SnakeandLadderUC1 {
 			}
 		}else if (options==2){
 			System.out.println("Snake");
-			temp=(position-rolldie);
+			position=position-rolldie;
 			System.out.println("Current position"+" "+position);
-			if (temp<0) {
+			if (position<0) {
 				position=0;
-			System.out.println("Current Position is"+" "+position);
+			System.out.println("New Position is"+" "+position);
 			}
-			}
-	    }
-		
+		}
 	}
+}
 }
 	
